1895d8dcf618fe1827d14021a76ad600cb7bf163
Michał Zakrzewski <michalzakrzewski@users.noreply.github.com>
2018-03-02 17:31:29 +0100

    new project

---
+++ src/main/java/pl/devmich/edziennik/EdziennikApplication.java
@@ -0,0 +1,12 @@
+package pl.devmich.edziennik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdziennikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdziennikApplication.class, args);
+	}
+}
