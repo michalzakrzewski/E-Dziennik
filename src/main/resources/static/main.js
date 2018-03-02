b156a9eb6a5a6174bd92bff7e9dcc86e0b1b002f
Michał Zakrzewski <michalzakrzewski@users.noreply.github.com>
2018-03-02 20:13:22 +0100

    delete, add and edit

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -1,17 +1,37 @@
 $(document).ready(function () {
-    $('.table .eBtn').on('click', function (event) {
+    $('.nBtn, .table .eBtn').on('click', function (event) {
 
         event.preventDefault(); // not send request
         var href = $(this).attr('href');
+        var text = $(this).text();
 
-        $.get(href, function (student, status) {
-            $('.myForm #id').val(student.id);
-            $('.myForm #firstName').val(student.firstName);
-            $('.myForm #lastName').val(student.lastName);
-            $('.myForm #age').val(student.age);
-            $('.myForm #degree').val(student.degree);
-        });
+        if (text == 'Edit'){
 
-        $('.myForm #exampleModal').modal();
+            $.get(href, function (student, status) {
+                $('.myForm #id').val(student.id);
+                $('.myForm #firstName').val(student.firstName);
+                $('.myForm #lastName').val(student.lastName);
+                $('.myForm #age').val(student.age);
+                $('.myForm #degree').val(student.degree);
+            });
+
+            $('.myForm #exampleModal').modal();
+        }else {
+            $('.myForm #id').val('');
+            $('.myForm #firstName').val('');
+            $('.myForm #lastName').val();
+            $('.myForm #age').val('');
+            $('.myForm #degree').val('');
+            $('.myForm #exampleModal').modal();
+        }
+    });
+
+    $('.table .delBtn').on('click', function (event) {
+        event.preventDefault();
+        var href = $(this).attr('href');
+        $('#myModal #delRef').attr('href', href);
+
+        $('#myModal').modal();
+
     });
 });
